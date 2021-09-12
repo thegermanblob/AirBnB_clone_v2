8ced19a65d6bc97d37babc8e3c559464f58a110b
Thegermanblob <thegermanblob@gmail.com>
2021-09-11 20:53:25 -0400

    Add task 3

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+""" flask app deployer module """
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """ function prints hello """
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """ Function prints hbnb"""
+    return("HBNB")
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c(text):
+    """ returns c with added text"""
+    return "C {}".format(text.replace("_", " "))
+
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python(text="is cool"):
+    """ returns c with added text"""
+    return "Python {}".format(text.replace("_", " "))
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0")
