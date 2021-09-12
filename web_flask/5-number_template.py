b5ceda0e10fcf7502694494165d5edb4150bc225
Thegermanblob <thegermanblob@gmail.com>
2021-09-11 21:30:53 -0400

    Make pep8 compliant

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -39,7 +39,7 @@ def number(n):
 @app.route('/number_template/<int:n>', strict_slashes=False)
 def number_template(n):
     """ returns n is a number if is a number """
-    return render_template('5-number.html', n = n)
+    return render_template('5-number.html', n=n)
 
 
 if __name__ == '__main__':
