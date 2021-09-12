b2c03223eed95fe5b533bf02ec1101d239b6b57b
Thegermanblob <thegermanblob@gmail.com>
2021-09-11 20:08:23 -0400

    Mke pep8 compliant

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
 from flask import Flask
 from flask.scaffold import F
-
+""" flask app dlepoyer"""
 app = Flask(__name__)
+
 
 @app.route('/')
 def hello_hbnb():
+    """ prints hello """
     return 'Hello HBNB!'
