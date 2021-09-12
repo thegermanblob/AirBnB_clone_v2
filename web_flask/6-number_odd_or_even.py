693e7fda201db323c8d48210ee3ca4677d942595
Thegermanblob <thegermanblob@gmail.com>
2021-09-12 15:35:23 -0400

    Add task 6

---
+++ web_flask/6-number_odd_or_even.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+""" flask app deployer module """
+from flask import Flask
+from flask import render_template
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """ function prints hello """
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """ Function prints hbnb"""
+    return("HBNB")
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c(text):
+    """ returns c with added text"""
+    return "C {}".format(text.replace("_", " "))
+
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python(text="is cool"):
+    """ returns python with added text"""
+    return "Python {}".format(text.replace("_", " "))
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def number(n):
+    """ returns n is a number if is a number """
+    return "{} is a number".format(n)
+
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def number_template(n):
+    """ returns n is a number if is a number """
+    return render_template('5-number.html', n=n)
+
+ 
+@app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
+def number_template(n):
+    """ returns n is a number if is a number """
+    return render_template('6-number_odd_or_even.html', n=n)
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0")
