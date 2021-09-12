fa13545d9d5ebcd435261fe722526a1d1af21665
Thegermanblob <thegermanblob@gmail.com>
2021-09-11 20:25:13 -0400

    Add task 1

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" flask app deployer module """
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """ function prints hello """
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    return("HBNB")
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0")
